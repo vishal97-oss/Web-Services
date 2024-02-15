330b4257f84e67cc134936b58132dc5f03d76d2c
Vishal Vaid <vai19004@byui.edu>
2024-02-15 13:49:43 -0700

    VIN

--- control/personal.js
+++ control/personal.js
@@ -60,5 +60,13 @@ const updatecarinfo = async (req, res) => {
   }
 };
 
+const getAll = async (req, res, next) => {
+  const result = await mongodb.getDb().db("CarDealership").collection('Cars').find();
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  });
+};
 
-module.exports = {createcarInfo, updatecarinfo, getSingle};
+
+module.exports = {createcarInfo, updatecarinfo, getSingle, getAll};
