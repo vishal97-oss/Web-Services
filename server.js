9947e91c9f8e9ca14fc2677d4b3a00433958cd25
Vishal Vaid <vai19004@byui.edu>
2024-02-27 21:39:03 -0700

    Week 8

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const mongodb1 = require('./DB/connect');
-
 const port = 8080;
 
 
@@ -21,3 +20,27 @@ app
     }
     //const port2 = 8080;
   });
+  // const port = process.env.PORT || 8080
+  const { auth } = require('express-openid-connect');
+  require('dotenv').config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+// app.listen(port,() => {
+//   console.log(`listening on port ${port}`)
+// })
