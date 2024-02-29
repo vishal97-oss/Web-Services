2b62f14620a54007b1f4a0c50c5d5b1098cb87c8
Vishal Vaid <vai19004@byui.edu>
2024-02-29 13:28:04 -0700

    Week 8

--- Routes/index.js
+++ Routes/index.js
@@ -2,21 +2,27 @@ const routes = require('express').Router();
 const contactController = require('../control/personal');
 // const mongodb = require('./DB/connect.js');
 const {carValidation} = require('../Validation/uservalidation');
+const { requiresAuth } = require('express-openid-connect');
 
 
-routes.post('/Cars', carValidation(), contactController.createcarInfo)
+routes.post('/Cars', requiresAuth() , carValidation(), contactController.createcarInfo)
 
-routes.put('/Cars/:id', carValidation(), contactController.updatecarinfo)
+routes.put('/Cars/:id' ,requiresAuth(), carValidation(), contactController.updatecarinfo)
 
-routes.get('/Cars/:id', contactController.getSingle )
+routes.get('/Cars/:id',requiresAuth(), contactController.getSingle )
 
-routes.get('/Cars', contactController.getAll)
+routes.get('/Cars',requiresAuth(), contactController.getAll)
 
 routes.use('/', require('./swagger'))
 
-routes.delete('/Cars/:id', contactController.deleteContact)
+routes.delete('/Cars/:id', requiresAuth(), contactController.deleteContact)
 
-
+routes.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        return res.status(401).send({msg: 'Invalid token'});
+    }
+    next(err, req, res);
+});
 
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -2,28 +2,16 @@ const express = require('express');
 const app = express();
 const mongodb1 = require('./DB/connect');
 const port = 8080;
-
+  // const port = process.env.PORT || 8080
+  const { auth } = require('express-openid-connect');
+  require('dotenv').config();
 
 const bodyParser = require('body-parser');
 // app.use(cors());
 app
   .use(bodyParser.json());
 
-  app.use('/', require('./Routes'));
-
-  mongodb1.initDb((err, mongodb1) => {
-    if (err) {
-      console.log(err);
-    } else {
-      app.listen(process.env.PORT || port);
-      console.log(`Connected to DB and listening on ${port}`);
-    }
-    //const port2 = 8080;
-  });
-  // const port = process.env.PORT || 8080
-  const { auth } = require('express-openid-connect');
-  require('dotenv').config();
-
+  
 const config = {
   authRequired: false,
   auth0Logout: true,
@@ -36,10 +24,26 @@ const config = {
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
+  app.use('/', require('./Routes'));
+
+  mongodb1.initDb((err, mongodb1) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(process.env.PORT || port);
+      console.log(`Connected to DB and listening on ${port}`);
+    }
+    //const port2 = 8080;
+  });
+
+
+
 // req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
+
+
 
 // app.listen(port,() => {
 //   console.log(`listening on port ${port}`)
